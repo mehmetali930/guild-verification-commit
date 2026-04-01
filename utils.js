7114985cb871d9e74a2c88be5f48d0787cca8fce
mehmetali930 <mehmetali930@gmail.com>
2026-04-01 19:58:44 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-01T16:57:32.173Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-04-01T16:58:43.396Z
+Commit: Fix broken links in documentation
