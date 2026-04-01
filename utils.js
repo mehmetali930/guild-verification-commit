82acdfd1529cc2096a69a15b103319af58f52ea6
mehmetali930 <mehmetali930@gmail.com>
2026-04-01 19:57:35 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:08:39.892Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-04-01T16:57:32.173Z
+Commit: Resolve timezone issues in date handling
