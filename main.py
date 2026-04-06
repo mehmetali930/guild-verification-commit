c731ac3b338609637800e34322cb5e011ad38a59
mehmetali930 <mehmetali930@gmail.com>
2026-04-06 09:01:18 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-06T04:55:25.447Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-06T06:01:17.243Z
+Commit: Fix broken links in documentation
